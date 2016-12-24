b309193342076f41e3d9616292364a976761e89b
rhynodegreat <rhynodegreat@gmail.com>
2016-12-24 16:19:27 -0600

    Implement Init and Terminate

--- vkalloc.cpp
+++ vkalloc.cpp
@@ -18,18 +18,34 @@ namespace vka {
     VkPhysicalDevice physicalDevice;
     VkDevice device;
     VkPhysicalDeviceMemoryProperties properties;
-    std::unordered_map<VkDeviceMemory, int32_t> pageMap;
+    std::unordered_map<VkDeviceMemory, int32_t> pageMap;    //caches index into one of the vector<Page> heaps
     std::vector<Page> heaps[32];    //32 possible heaps
 }
 
 void vkaInit(VkPhysicalDevice physicalDevice, VkDevice device){
+    vka::physicalDevice = physicalDevice;
+    vka::device = device;
 
 }
 
 using namespace vka;
 
 void vkaTerminate(){
+    pageMap.clear();
 
+    for (size_t i = 0; i < 32; i++) {
+        for (size_t j = 0; j < heaps[i].size(); j++) {
+            Node* current = heaps[i][j].head;
+
+            while (current){
+                Node* next = current->next;
+                delete current;
+                current = next;
+            }
+        }
+
+        heaps[i].clear();
+    }
 }
 
 VkAllocation vkAlloc(VkMemoryRequirements requirements){
